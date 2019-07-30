4f2ece6782185b913473044f3bda95c9f6671fce
nicolaasuni <info@tecnick.com>
2019-07-30 19:45:52 +0100

    Fix Go dependency

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='variantkey',
-    version='5.4.2.1',
+    version='5.4.3.1',
     keywords=('variantkey variant key genetic genomics'),
     description="VariantKey Bindings for Python",
     long_description=read('../README.md'),
