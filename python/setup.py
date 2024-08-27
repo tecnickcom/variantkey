9231a67123cad15ea88d4a3048fa6a0e9a10828d
nicolaasuni <info@tecnick.com>
2024-08-27 12:27:17 +0100

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="variantkey",
-    version="5.6.15.0",
+    version="5.6.16.0",
     keywords=("variantkey variant key genetic genomics"),
     description="VariantKey Bindings for Python",
     long_description=read("../README.md"),
