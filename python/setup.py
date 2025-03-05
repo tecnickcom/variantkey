0d1eb81868b4be72fdb639207173ee6105b3fd2b
nicolaasuni <info@tecnick.com>
2025-03-05 07:55:45 +0000

    Update all dependencies

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="variantkey",
-    version="5.6.28.0",
+    version="5.6.29.0",
     keywords=("variantkey variant key genetic genomics"),
     description="VariantKey Bindings for Python",
     long_description=read("../README.md"),
