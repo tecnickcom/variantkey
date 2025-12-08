65597f3cc1fea3d88f2b3a3f7800b72d2cf053dc
Nicola Asuni <info@tecnick.com>
2025-12-08 19:31:31 +0000

    update dependencies

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/tecnickcom/variantkey
 
 go 1.25
 
-toolchain go1.25.4
+toolchain go1.25.5
 
 require (
 	github.com/jstemmer/go-junit-report/v2 v2.1.0
--- python-class/setup.py
+++ python-class/setup.py
@@ -38,7 +38,7 @@ class RunTests(Command):
 
 setup(
     name="pyvariantkey",
-    version="5.7.13.0",
+    version="5.7.14.0",
     keywords=("variantkey variant key genetic genomics"),
     description="VariantKey Python wrapper class",
     long_description=read("../README.md"),
@@ -55,7 +55,7 @@ setup(
     ],
     install_requires=[
         "numpy>=1.24.2,<2",
-        "variantkey>=5.7.13.0",
+        "variantkey>=5.7.14.0",
     ],
     extras_require={
         "test": [
